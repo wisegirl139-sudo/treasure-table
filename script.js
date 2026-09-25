3ffe965e9726a78c895b5ae1623ece45de46e43b
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-25 23:21:35 +0100

    EHY DOES IT KEEP DISAPEARINGGGGG

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let wave =document.getElementById("wave");
+let st=-window.innerHeight*0.8;
+wave.style.top="-80%";
 window.addEventListener("scroll",function(){
-    wave.style.top=(-1500+window.scrollY/2)+"px";
+    wave.style.top=(st+window.scrollY*1)+"px";
 });
