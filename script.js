3a3dc970067d8eacaa42f6bc5a5d24cd583245a1
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-26 00:27:36 +0100

    oooo adding more html for the adding components part

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let wave =document.getElementById("wave");
 let st=-window.innerHeight*0.8;
 wave.style.top="20vh";
 window.addEventListener("scroll",function(){
-    wave.style.top=(20+window.scrollY/window.innerHeight*300)+"vh";
+    wave.style.top=(20+window.scrollY/window.innerHeight*100)+"vh";
 });
