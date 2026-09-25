61da49580a00c4fc9d0c986f532dd65eb17a9b26
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-26 00:01:02 +0100

    okay so the p is jsut bellow the title , also added a new font and madeit show more when u scroll, will acc do more like 350 tbh

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let wave =document.getElementById("wave");
 let st=-window.innerHeight*0.8;
-wave.style.top="-80%";
+wave.style.top="20vh";
 window.addEventListener("scroll",function(){
-    wave.style.top=(st+window.scrollY*1)+"px";
+    wave.style.top=(20+window.scrollY/window.innerHeight*300)+"vh";
 });
