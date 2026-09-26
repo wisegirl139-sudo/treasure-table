dd0569833c17040fbe35f2fae39c869c59807f58
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-26 20:30:11 +0100

    it didnt work but im read up on more of the@ i onle ever used them when making my signature moving stuff but i just read that they can help with phone layouts(the js failed kinda anyways gonna tyr that )

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ let wave =document.getElementById("wave");
 let st=-window.innerHeight*0.8;
 wave.style.top="20vh";
 window.addEventListener("scroll",function(){
-    
     let pos=20+window.scrollY/window.innerHeight*100;
-    if(pos>100){
+    if(pos>150){
         pos=100;
-    }
+    };
     wave.style.top=pos+"vh";
 });
