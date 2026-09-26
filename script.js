d86472606c9ea4b89a5804e95cc6867bae340cf6
wisegirl139-sudo <wisegirl139@gmail.com>
2026-09-26 19:40:51 +0100

    was testing physical bounds on it so now we just control transform instead of the overall scroll based on 1 starting position , oliver pointed out how odd it looked on phones so this should help im gonna make the the wave come down lower rn tho bc u can see it and i wanna give it a washed away look

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ let wave =document.getElementById("wave");
 let st=-window.innerHeight*0.8;
 wave.style.top="20vh";
 window.addEventListener("scroll",function(){
-    wave.style.top=(20+window.scrollY/window.innerHeight*100)+"vh";
+    
+    let pos=20+window.scrollY/window.innerHeight*100;
+    if(pos>100){
+        pos=100;
+    }
+    wave.style.top=pos+"vh";
 });
